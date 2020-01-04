2963ed283c9d4f12201d667b2f26e37ab286e10f
Jordan Hall <jordan@hall05.co.uk>
2020-01-04 22:40:20 +0000

    Fully comment main class

--- src/AttemptHandler.php
+++ src/AttemptHandler.php
@@ -8,23 +8,70 @@ use DivineOmega\Attempt\Exceptions\DateTimeExceeded;
 use DivineOmega\Attempt\Exceptions\MaxAttemptsExceeded;
 use Exception;
 
+/**
+ * Class AttemptHandler
+ * @package DivineOmega\Attempt
+ */
 class AttemptHandler
 {
+    /**
+     * Function to attempt
+     *
+     * @var callable
+     */
     private $function;
+
+    /**
+     * Maximum number of attempts before giving up (0 = no limit)
+     *
+     * @var int
+     */
     private $maxAttempts = 0;
+
+    /**
+     * Date time to give up at, or null to not give up based on time
+     *
+     * @var DateTimeInterface|null
+     */
     private $dateTime = null;
+
+    /**
+     * Gap (delay) between attempts in seconds
+     *
+     * @var int
+     */
     private $gap = 0;
 
+    /**
+     * AttemptHandler constructor.
+     *
+     * @param callable $function
+     */
     public function __construct(callable $function)
     {
         $this->function = $function;
     }
 
+    /**
+     * Starts attempting to run the function immediately.
+     *
+     * @return mixed
+     * @throws DateTimeExceeded
+     * @throws MaxAttemptsExceeded
+     */
     public function now()
     {
         return $this->callFunction();
     }
 
+    /**
+     * Waits until a specified date and time, then starts attempting to run the function.
+     *
+     * @param DateTimeInterface $dateTime
+     * @return mixed
+     * @throws DateTimeExceeded
+     * @throws MaxAttemptsExceeded
+     */
     public function at(DateTimeInterface $dateTime)
     {
         while (true) {
@@ -38,6 +85,14 @@ class AttemptHandler
         return $this->now();
     }
 
+    /**
+     * Handles calling the specified function with the specified gap (delay)
+     * until the max attempts is reached or the specified date time has passed.
+     *
+     * @return mixed
+     * @throws DateTimeExceeded
+     * @throws MaxAttemptsExceeded
+     */
     private function callFunction()
     {
         $attempts = 0;
@@ -72,18 +127,36 @@ class AttemptHandler
         }
     }
 
+    /**
+     * Specifies the maximum number of attempts
+     *
+     * @param int $maxAttempts
+     * @return $this
+     */
     public function maxAttempts(int $maxAttempts)
     {
         $this->maxAttempts = $maxAttempts;
         return $this;
     }
 
+    /**
+     * Specifies the date time to attempt until
+     *
+     * @param DateTimeInterface $dateTime
+     * @return $this
+     */
     public function until(DateTimeInterface $dateTime)
     {
         $this->dateTime = $dateTime;
         return $this;
     }
 
+    /**
+     * Specifies the gap (delay) between each attempt, in seconds
+     *
+     * @param int $gap
+     * @return $this
+     */
     public function withGap(int $gap)
     {
         $this->gap = $gap;
